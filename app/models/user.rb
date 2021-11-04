41acdcd2cefcdd91bdaa3c9000190c04f650de51
hlomochi1023 <s11ag065@yahoo.co.jp>
2021-11-04 11:21:45 +0900

    ユーザー管理機能修正（1回目）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # has_many :buyers
-  has_many :items
-  has_many :buyers
+  # has_many :items
+  # has_many :buyers
 
   validates :nickname, presence: true
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
@@ -13,5 +13,5 @@ class User < ApplicationRecord
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/ }
   validates :birthday, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
