5851dae52e00d151e11339ae3187277b12deb464
hlomochi1023 <s11ag065@yahoo.co.jp>
2021-11-06 21:02:46 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,65 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :item_edit, only: [:edit, :update]
+  before_action :sold_out, only: [:edit]
+
+  def index
+    @items = Item.all.order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :condition_id, :category_id, :delivery_charge_id, :delivery_day_id,
+                                 :prefecture_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_edit
+    redirect_to root_path unless current_user == @item.user
+  end
+
+  def sold_out
+    redirect_to root_path unless @item.buyer.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root to: "items#index"
 
-  #resources :items do
-    #resources :buyers, only: [:index, :create]
-    #end
+  resources :items do
+  resources :buyers, only: [:index, :create]
+    end
 end
